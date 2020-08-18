2ed6e6e590c5ae1b68eb39569eaaccd33907895a
D-Gale <d.l.gale.91@gmail.com>
2020-08-18 02:33:30 +0100

    Syntax fixing

--- Tutorials/Variables/app.py
+++ Tutorials/Variables/app.py
@@ -1,8 +1,8 @@
 price=10
-print('printing out the Integer for price',price)
+print('printing out the Integer for price', price)
 rating = 4.9
-print('printing out the Float value for rating',rating)
+print('printing out the Float value for rating', rating)
 name = 'Dan'
-print('printing out the String name for name',name)
+print('printing out the String name for name', name)
 is_published = False
-print('printing out the Boolean for is_published note; the variables are underscrolled when 2 words and True and False need to be capitalised :',is_published)
+print('printing out the Boolean for is_published note; the variables are underscrolled when 2 words and True and False need to be capitalised :', is_published)
