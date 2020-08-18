20c7f26584e57ca953ea718e45ee43ccb5c641d2
D-Gale <d.l.gale.91@gmail.com>
2020-08-18 02:33:01 +0100

    2 Tutorials

---
+++ Tutorials/Variables/exercise.py
@@ -0,0 +1,11 @@
+print('Excerise: We check in a patient named John Smith. He is 20 years old and is a new patient')
+print('Set up variables to take his name, age and if he is an old or new patient')
+
+name = 'John Smith'
+age = 20
+is_new = True
+
+print('Printing out patient Information: ')
+print('Patients name is: ', name)
+print('Patients age is: ', age)
+print('Is this patient new?', is_new)
